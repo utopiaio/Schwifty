0b8fcbbd6b0d92e6ca8a596e137ba1581f39c962
Moe Szyslak <moe.duffdude@gmail.com>
2017-04-29 12:37:12 +0300

    id and scopes moved to config

---
+++ app/config/spotify.js
@@ -0,0 +1,9 @@
+const CLIENT_ID = '8ad7a6f78ef9487a92183ea076446e27';
+const REDIRECT_URI = 'http://localhost:8080/';
+const SCOPE = 'playlist-modify-public';
+
+module.exports = {
+  CLIENT_ID,
+  REDIRECT_URI,
+  SCOPE,
+};
