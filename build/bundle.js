69cd3fdcb38a923e0b898aa58d17b79501b5aceb
Moe Szyslak <moe.duffdude@gmail.com>
2017-05-19 17:08:55 +0300

    redirect url update

--- build/bundle.js
+++ build/bundle.js
@@ -243,7 +243,7 @@ module.exports = config;
 
 const CLIENT_ID = '8ad7a6f78ef9487a92183ea076446e27';
 // const REDIRECT_URI = 'http://localhost:8080/'; // [LOCAL]
-const REDIRECT_URI = 'https://moe-szyslak.github.io/Schwifty/'; // [GITHUB]
+const REDIRECT_URI = 'https://utopiaio.github.io/Schwifty/'; // [GITHUB]
 const SCOPE = 'playlist-modify-public';
 
 module.exports = {
